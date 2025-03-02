bdf759adba6ccb001cc964f9ad3970d58344283d
gowtham <gowthamgalaxy007@gmail.com>
2025-03-02 21:57:57 +0530

    api call updated

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: process.env.NEXT_API_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
