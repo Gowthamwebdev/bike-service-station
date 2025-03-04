5d623ab477136c711aa6ae0828737601167d0261
gowtham <gowthamgalaxy007@gmail.com>
2025-03-04 23:12:13 +0530

    api conn updated

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "https://bike-service-station-server.vercel.app",
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
